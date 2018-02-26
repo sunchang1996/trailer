9f0ef410fd94553165ce3a2516691f0f2d5c3482
sunchang <sunchang612@gmail.com>
2018-02-26 23:34:43 +0800

    feat: 增加模板引擎中间件

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # trailer
 koa2 搭建网站预告片
+
+- extends ./layouts/default 
+> 继承
+
+- block 声明一个块级变量
--- server/index.js
+++ server/index.js
@@ -1,13 +1,19 @@
 const Koa = require('koa');
 const app = new Koa();
-const { htmlTpl, ejsTpl } = require('./tpl')
-const ejs = require('ejs')
+// const { htmlTpl, ejsTpl, pugTpl } = require('./tpl')
+// const ejs = require('ejs')
+// const pug = require('pug')
 
+const views = require('koa-views')
+const { resolve } = require('path')
+
+app.use(views(resolve(__dirname, './views'), {
+  extension: 'pug' // 识别扩展名是pug的文件
+}))
 app.use(async (ctx, next) => {
-  ctx.type = 'text/html; charset=utf-8';
-  ctx.body = ejs.render(ejsTpl, {
-    you: 'hi',
-    me: 'koa2'
+  await ctx.render('index', {
+    you: 'koa',
+    me: 'word'
   })
 })
 
