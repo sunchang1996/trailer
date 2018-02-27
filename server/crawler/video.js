af629bd9586d1e2981033be92832208ee28fa52f
sunchang <sunchang612@gmail.com>
2018-02-27 17:40:17 +0800

    feat: 新增预告片详情页视频和封面图爬取功能

--- server/crawler/video.js
+++ server/crawler/video.js
@@ -1,67 +1,73 @@
-
-const url = `https://movie.douban.com/tag/#/?sort=T&range=5,10&tags=`
-
 const puppeteer = require('puppeteer')
 
-// 添加一个等待时间的定时器
+const base = `https://movie.douban.com/subject/`
+const doubanId = '26739551'
+const videoBase = `https://movie.douban.com/trailer/219491`
+
 const sleep = time => new Promise(resolve => {
-  setTimeout(resolve, time);
+  setTimeout(resolve, time)
 })
 
 ;(async () => {
-  console.log('start')
+  console.log('Start visit the target page')
 
   const browser = await puppeteer.launch({
-    args: ['--no--sandbox'],
-    dumpio: false,
+    args: ['--no-sandbox'],
+    dumpio: false
   })
 
-  const page = await browser.newPage() // 开启一个新的页面
-  await page.goto(url, {
+  const page = await browser.newPage()
+  await page.goto(base + doubanId, {
     waitUntil: 'networkidle2'
   })
 
   await sleep(1000)
 
-  await page.waitForSelector('.more')
-
-  // 加载两页数据 因为有一个加载更多的按钮 手动点击执行一次
-  for(let i = 0; i< 1; i++) {
-    await sleep(3000)
-    await page.click('.more')
-  }
-
   const result = await page.evaluate(() => {
-    var $ = window.$;
-    
-    var items = $('.list-wp a') // 用jQuery拿到标签
-    var links = []
+    var $ = window.$
+    var it = $('.related-pic-video')
 
-    if (items.length >= 1) {
-      items.each((index, item) => {
-        let it = $(item)
-        // 影片id
-        let doubanId = it.find('div').data('id')
-        // 影片标题
-        let title = it.find('.title').text()
-        // 拿到影片的评级
-        let rate = Number(it.find('.rate').text())
-        // 拿到页面上的图片地址，把图片替换成大图片
-        let poster = it.find('img').attr('src').replace('s_ratio', 'l_ratio')
+    if (it && it.length > 0) {
+      var link = it.attr('href')
+      var cover = it.find('img').attr('src')
 
-        links.push({
-          doubanId,
-          title,
-          rate,
-          poster
-        })
-      })
+      return {
+        link,
+        cover
+      }
     }
-    return links;
+
+    return {}
   })
 
-  browser.close();// 关闭浏览器
+  let video
 
-  process.send({ result }) // 发送出去
+  if (result.link) {
+    await page.goto(result.link, {
+      waitUntil: 'networkidle2'
+    })
+    await sleep(2000)
+
+    video = await page.evaluate(() => {
+      var $ = window.$
+      var it = $('source')
+
+      if (it && it.length > 0) {
+        return it.attr('src')
+      }
+
+      return ''
+    })
+  }
+
+  const data = {
+    video,
+    doubanId,
+    cover: result.cover
+  }
+
+  browser.close()
+
+  process.send(data)
   process.exit(0)
 })()
