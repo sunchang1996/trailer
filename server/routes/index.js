904d7aef94b92ec5291f7c1544103535ebbd6330
sunchang <sunchang612@gmail.com>
2018-04-22 20:39:07 +0800

    feat: 添加装饰器

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,27 +1,63 @@
 const Router = require('koa-router');
 const mongoose = require('mongoose');
 const router = new Router();
+// const router = new Router({
+//   prefix: '/movies' // 加router的公用前缀
+// });
 
-// 获取所有的电影列表
-router.get('/movies/all', async (ctx, next) => {
-  const Movie = mongoose.model('Movie');
-  const movies = await Movie.find({}).sort({
-    'meta.createdAt': -1
-  })
-  ctx.body= {
-    movies
+// 声明名一个装饰器
+@controller('/api/v0/movies')
+export class movieController{
+  get('/')
+  // @login 可以添加其他的装饰器
+  // @log
+  // @required({body: })
+  async getMovies (ctx, next) => {
+    const Movie = mongoose.model('Movie');
+    const movies = await Movie.find({}).sort({
+      'meta.createdAt': -1
+    })
+    ctx.body= {
+      movies
+    }
   }
-})
+  @get('/:id')
+  async getMoviesDetail(ctx,next) {
+    const Movie = mongoose.model('Movie');
+    const id = ctx.params.id;
+    const movie = await Movie.findOne({_id: id });
 
-// 获取单个电影
-router.get('/movies/detail/:id', async(ctx, next) => {
-  const Movie = mongoose.model('Movie');
-  const id = ctx.params.id;
-  const movie = await Movie.findOne({_id: id });
-
-  ctx.body = {
-    movie
+    ctx.body = {
+      movie
+    }
   }
-}) 
+}
+
+
+
+
+
+
+// // 获取所有的电影列表
+// router.get('/movies/all', async (ctx, next) => {
+//   const Movie = mongoose.model('Movie');
+//   const movies = await Movie.find({}).sort({
+//     'meta.createdAt': -1
+//   })
+//   ctx.body= {
+//     movies
+//   }
+// })
+
+// // 获取单个电影
+// router.get('/movies/detail/:id', async(ctx, next) => {
+//   const Movie = mongoose.model('Movie');
+//   const id = ctx.params.id;
+//   const movie = await Movie.findOne({_id: id });
+
+//   ctx.body = {
+//     movie
+//   }
+// }) 
 
 module.exports = router;
